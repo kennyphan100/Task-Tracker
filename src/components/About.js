6c3a503895d5ab44bab470c94a07aadae3537ae4
Kenny <kennyphan100@gmail.com>
2022-01-01 22:34:51 -0500

    Deploy application to GitHub Pages

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+            <Link to='/'> Go Back </Link>
+            <h4>This application is a project management tool that allows you to keep track of your tasks.</h4>
+        </div>
+    )
+}
+
+export default About
